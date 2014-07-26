fae1a516b491b6af0b38c5a006c4340c5896fb75
tsntsumi <tsntsumi@tsntsumi.com>
2014-07-26 13:47:04 +0900

    Initial Commit

---
+++ SingletonPatternInSwift/SingletonPatternInSwift.h
@@ -0,0 +1,19 @@
+//
+//  SingletonPatternInSwift.h
+//  SingletonPatternInSwift
+//
+//  Created by 堤 紀久夫 on 2014/07/26.
+//  Copyright (c) 2014年 tsntsumi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SingletonPatternInSwift.
+FOUNDATION_EXPORT double SingletonPatternInSwiftVersionNumber;
+
+//! Project version string for SingletonPatternInSwift.
+FOUNDATION_EXPORT const unsigned char SingletonPatternInSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SingletonPatternInSwift/PublicHeader.h>
+
+
